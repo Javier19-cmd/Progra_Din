cfce6040674d4c7fec92c06f14f49d22fd777d4f
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2023-04-03 16:24:22 -0600

    :v:

--- hanoi.py
+++ hanoi.py
@@ -1,10 +1,14 @@
-def moverTorre(altura,origen, destino, intermedio):
-    if altura >= 1:
-        moverTorre(altura-1,origen,intermedio,destino)
-        moverDisco(origen,destino)
-        moverTorre(altura-1,intermedio,destino,origen)
+def moverTorre(altura, origen, destino, intermedio, memo):
+    if altura == 1:
+        print("mover disco de", origen, "a", destino)
+        return
+    if (altura, origen, destino, intermedio) in memo:
+        return memo[(altura, origen, destino, intermedio)]
+    
+    moverTorre(altura-1, origen, intermedio, destino, memo)
+    print("mover disco de", origen, "a", destino)
+    memo[(altura, origen, destino, intermedio)] = True
+    moverTorre(altura-1, intermedio, destino, origen, memo)
 
-def moverDisco(desde,hacia):
-    print("mover disco de",desde,"a",hacia)
-
-moverTorre(3,"A","B","C")
+memo = {}
+moverTorre(3, "A", "B", "C", memo)
